44712721eccea3e54e5b1a001c05fd4312032377
paploxd-svg <paploxd@gmail.com>
2026-03-12 19:42:54 -0700

    nose

--- blog/models.py
+++ blog/models.py
@@ -21,5 +21,24 @@ class Post(models.Model):
         {DRAFT, 'Draft'}
     }
 
-    category = models.ForeignKey(category, related_name='posts', on_delete=models.)
-    title  = models.
+    category = models.ForeignKey(category, related_name='posts', on_delete=models.CASCADE)
+    title  = models.CharField(max_length=255)
+    intro = models.TextField()
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=10, choices=CHOISES_STATUS, default=ACTIVATE)
+    image = models.ImageField(upload_to='upload/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    name  = models.CharField(max_length=255)
+    email = models.EmailField()
+    body = models.TextField()
+    created_ad = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+    
