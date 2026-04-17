6efc8db21274c51b7307fe88687f6ada8bbf6af1
paploxd-svg <paploxd@gmail.com>
2026-04-16 18:57:47 -0700

    ss

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 from .models import Comment
 
-class CommentForm(forms.ModelsForm):
+class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['name', 'email', 'body']
